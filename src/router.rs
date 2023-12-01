6a83111198402055cf7c48db035126cd1bb8282f
razzdrgn <razzdrgn@protonmail.com>
2023-12-01 07:49:09 -0500

    day one initial response

--- src/router.rs
+++ src/router.rs
@@ -1,9 +1,16 @@
-use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
+use axum::{
+	extract::Path,
+	http::StatusCode,
+	response::IntoResponse,
+	Router,
+	routing::get
+};
 
 pub fn build_router() -> Router {
 	Router::new()
 		.route("/", get(root_endpoint))
 		.route("/-1/error", get(day_minusone))
+		.route("/1/*serial", get(day_one))
 }
 
 pub async fn root_endpoint() -> impl IntoResponse {
@@ -16,4 +23,13 @@ pub async fn root_endpoint() -> impl IntoResponse {
 
 pub async fn day_minusone() -> impl IntoResponse {
 	(StatusCode::INTERNAL_SERVER_ERROR, "B-B-B-B-BONUS!!!").into_response()
+}
+
+pub async fn day_one(Path(serial): Path<String>) -> impl IntoResponse {
+	let v: Vec<&str> = serial.split_terminator('/').collect();
+	let mut result: u32 = 0;
+	for i in v.iter() {
+		result = result ^ i.parse::<u32>().unwrap_or(0);
+	}
+	(StatusCode::OK, result.pow(3).to_string()).into_response()
 }
