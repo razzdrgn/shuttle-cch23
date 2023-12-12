28692d48d6ea4f4a31e2c4c44f51434c78b15b64
razzdrgn <razzdrgn@protonmail.com>
2023-12-12 18:00:37 -0500

    day 12 challenge 3

--- src/advent/day12.rs
+++ src/advent/day12.rs
@@ -1,35 +1,94 @@
-use axum::{extract::{State, Path}, response::IntoResponse, Json};
+use axum::{
+	extract::{Path, State},
+	response::IntoResponse,
+	Json,
+};
+use chrono::prelude::*;
+use serde::Serialize;
 use serde_json::Value;
+use std::{
+	collections::HashMap,
+	sync::{Arc, RwLock},
+	time::Instant,
+};
 use tracing::info;
-use std::{sync::{Arc, RwLock}, time::Instant, collections::HashMap};
-use uuid::Uuid;
 use ulid::Ulid;
+use uuid::Uuid;
 
 pub fn router() -> axum::Router {
-	let state = Arc::new(RwLock::new(TimeState {map: HashMap::new()}));
+	let state = Arc::new(RwLock::new(TimeState {
+		map: HashMap::new(),
+	}));
 
 	axum::Router::new()
 		.route("/12", axum::routing::get(axum::http::StatusCode::OK))
 		.route("/12/save/:key", axum::routing::post(save_key))
 		.route("/12/load/:key", axum::routing::get(fetch_key))
 		.route("/12/ulids", axum::routing::post(day_twelve_ulids))
-		.route("/12/ulids/:day", axum::routing::post(axum::http::StatusCode::NOT_IMPLEMENTED))
+		.route("/12/ulids/:day", axum::routing::post(day_twelve_lsb))
 		.with_state(state)
 }
 
 struct TimeState {
-	map: HashMap<String, Instant>
+	map: HashMap<String, Instant>,
 }
 
-async fn save_key(State(state): State<Arc<RwLock<TimeState>>>, Path(key): Path<String>) -> impl IntoResponse {
-	state.write().expect("Could not access shared state as writeable").map.insert(key, Instant::now());
+#[derive(Serialize)]
+struct LsbOutput {
+	#[serde(rename(serialize = "christmas eve"))]
+	christmas_eve: usize,
+	weekday: usize,
+	#[serde(rename(serialize = "in the future"))]
+	future: usize,
+	#[serde(rename(serialize = "LSB is 1"))]
+	lsb: usize,
+}
+
+impl LsbOutput {
+	fn new(i: &[Ulid], day: u32) -> LsbOutput {
+		let dates = i
+			.iter()
+			.map(Ulid::datetime)
+			.map(DateTime::<Utc>::from);
+
+		let christmas_eve = dates.clone().filter(|d| d.month() == 12 && d.day() == 24).count();
+		let weekday = dates.clone().filter(|d| d.weekday().num_days_from_sunday() == day).count();
+		let future = dates.filter(|d| d.gt(&Utc::now())).count();
+		let lsb = i.iter().filter(|u| u.0 & 1 == 1).count();
+
+		LsbOutput {
+			christmas_eve,
+			weekday,
+			future,
+			lsb
+		}
+	}
+}
+
+async fn save_key(
+	State(state): State<Arc<RwLock<TimeState>>>,
+	Path(key): Path<String>,
+) -> impl IntoResponse {
+	state
+		.write()
+		.expect("Could not access shared state as writeable")
+		.map
+		.insert(key, Instant::now());
 	axum::http::StatusCode::OK
 }
 
-async fn fetch_key(State(state): State<Arc<RwLock<TimeState>>>, Path(key): Path<String>) -> impl IntoResponse {
-	let state = state.read().expect("Could not access shared state as readble");
+async fn fetch_key(
+	State(state): State<Arc<RwLock<TimeState>>>,
+	Path(key): Path<String>,
+) -> impl IntoResponse {
+	let state = state
+		.read()
+		.expect("Could not access shared state as readble");
 	let earlier = state.map.get(&key).unwrap();
-	(axum::http::StatusCode::OK, format!("{}", Instant::now().duration_since(*earlier).as_secs()))
+	(
+		axum::http::StatusCode::OK,
+		format!("{}", Instant::now().duration_since(*earlier).as_secs()),
+	)
 }
 
 async fn day_twelve_ulids(Json(body): Json<Value>) -> impl IntoResponse {
@@ -37,4 +96,14 @@ async fn day_twelve_ulids(Json(body): Json<Value>) -> impl IntoResponse {
 	let inputs: Vec<Ulid> = serde_json::from_value(body).expect("Could not deserialize input");
 	let outputs: Vec<Uuid> = inputs.into_iter().rev().map(Uuid::from).collect();
 	(axum::http::StatusCode::OK, Json(outputs))
+}
+
+async fn day_twelve_lsb(Path(day): Path<u32>, Json(body): Json<Value>) -> impl IntoResponse {
+	if day > 6 {
+		return axum::http::StatusCode::BAD_REQUEST.into_response();
+	};
+	info!("Request Body: {body}");
+	let inputs: Vec<Ulid> = serde_json::from_value(body).expect("Could not deserialize input");
+
+	(axum::http::StatusCode::OK, Json(LsbOutput::new(&inputs, day))).into_response()
 }
