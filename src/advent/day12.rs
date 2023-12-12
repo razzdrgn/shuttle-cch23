94679cffd23f371b92bcfa2b1fe1adc2be559934
razzdrgn <razzdrgn@protonmail.com>
2023-12-12 16:48:23 -0500

    day 12 challenge 2

--- src/advent/day12.rs
+++ src/advent/day12.rs
@@ -1,5 +1,9 @@
-use axum::{extract::{State, Path}, response::IntoResponse};
+use axum::{extract::{State, Path}, response::IntoResponse, Json};
+use serde_json::Value;
+use tracing::info;
 use std::{sync::{Arc, RwLock}, time::Instant, collections::HashMap};
+use uuid::Uuid;
+use ulid::Ulid;
 
 pub fn router() -> axum::Router {
 	let state = Arc::new(RwLock::new(TimeState {map: HashMap::new()}));
@@ -8,7 +12,7 @@ pub fn router() -> axum::Router {
 		.route("/12", axum::routing::get(axum::http::StatusCode::OK))
 		.route("/12/save/:key", axum::routing::post(save_key))
 		.route("/12/load/:key", axum::routing::get(fetch_key))
-		.route("/12/ulids", axum::routing::post(axum::http::StatusCode::NOT_IMPLEMENTED))
+		.route("/12/ulids", axum::routing::post(day_twelve_ulids))
 		.route("/12/ulids/:day", axum::routing::post(axum::http::StatusCode::NOT_IMPLEMENTED))
 		.with_state(state)
 }
@@ -26,4 +30,11 @@ async fn fetch_key(State(state): State<Arc<RwLock<TimeState>>>, Path(key): Path<
 	let state = state.read().expect("Could not access shared state as readble");
 	let earlier = state.map.get(&key).unwrap();
 	(axum::http::StatusCode::OK, format!("{}", Instant::now().duration_since(*earlier).as_secs()))
+}
+
+async fn day_twelve_ulids(Json(body): Json<Value>) -> impl IntoResponse {
+	info!("Request Body: {body}");
+	let inputs: Vec<Ulid> = serde_json::from_value(body).expect("Could not deserialize input");
+	let outputs: Vec<Uuid> = inputs.iter().rev().map(|u| Uuid::from(*u)).collect();
+	(axum::http::StatusCode::OK, Json(outputs))
 }
